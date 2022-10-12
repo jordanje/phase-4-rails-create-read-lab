3fe6b578fd3694f2e132a481a295d98680a97429
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-12 11:41:39 -0400

    commit tree 80bbd61e492b42eddd4cf25cb225468c97a8a145

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,16 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all 
+        render json: plants
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        render json: plant
+    end
+
+    def create
+        new_plant = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+        render json: new_plant, status: 201
+    end
+end
